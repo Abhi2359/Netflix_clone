c50c00eea73446666bb78402206a2766ab6ca8ea
Abhi2359 <abhishekchoudhary1018@gmail.com>
2022-11-01 20:28:09 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./app.scss"
 import Home from "./pages/home/Home";
 import Watch from './pages/watch/Watch';
+import Login from "./pages/login/Login";
 import Register from './pages/register/Register';
+import Featured from "./components/featured/Featured";
 
 
 function App() {
-  return <Register/>
+  return <Featured/>
 }
 
 export default App;
